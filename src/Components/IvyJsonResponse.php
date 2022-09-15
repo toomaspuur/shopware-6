c2ba249e20289fa4e6be9048416c8146b2635a03
toomas puur <toomas.puur@hammercode.eu>
2022-09-15 21:40:56 +0200

    https://trello.com/c/CWeHpnUZ/66-shopware-6-umlaute-und-%C3%9F-zeichen-werden-in-jsonresponse-anders-signiert-als-bei-uns

---
+++ src/Components/IvyJsonResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace WizmoGmbh\IvyPayment\Components;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class IvyJsonResponse extends JsonResponse
+{
+    /**
+     * @param $data
+     * @param int $status
+     * @param array $headers
+     * @param bool $json
+     */
+    public function __construct($data = null, int $status = 200, array $headers = [], bool $json = false)
+    {
+        parent::__construct($data, $status, $headers, $json);
+        $this->encodingOptions = $this->encodingOptions | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+    }
+}
