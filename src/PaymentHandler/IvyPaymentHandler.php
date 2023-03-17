693072195920fee68b6abd0478486d59aef690a3
kostoman <805344+kostoman@users.noreply.github.com>
2023-03-17 11:09:59 +0100

    fix: move logger into try

--- src/PaymentHandler/IvyPaymentHandler.php
+++ src/PaymentHandler/IvyPaymentHandler.php
@@ -111,8 +111,8 @@ class IvyPaymentHandler implements AsynchronousPaymentHandlerInterface
             $this->logger->info('pseudo pay: immediately return url to finalize transaction');
             return new RedirectResponse($returnUrl);
         } else {
-            $this->logger->info('checkout needs to be created with orderId: '. $transaction->getOrder()->getId());
             try {
+                $this->logger->info('checkout needs to be created with orderId: '. $transaction->getOrder()->getId());
                 $order = $this->getOrderById($transaction->getOrder()->getId(), $salesChannelContext);
                 $returnUrl = $this->ivyCheckoutSession->createCheckoutSession(
                     $contextToken,
